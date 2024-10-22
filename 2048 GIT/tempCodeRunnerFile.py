4f8e02b5d62f441791c598335f75a8cf45f1de11
Zargul <Zargul.ansari373@gmail.com>
2024-10-22 18:15:37 +0500

    Add files via upload

---
+++ 2048 GIT/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+def merge(mat, done):
+    for i in range(c.GRID_LEN):
+        for j in range(c.GRID_LEN-1):
+            if mat[i][j] == mat[i][j+1] and mat[i][j] != 0:
+                mat[i][j] *= 2
+                mat[i][j+1] = 0
+                done = True
+    return mat, done
